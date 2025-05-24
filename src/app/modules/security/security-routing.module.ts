474c9bad5fd39f8cf686cff142bef6177501a65a
DilshanMP <mpdilshan.99@gmail.com>
2025-05-24 23:06:05 +0530

    confid add

--- src/app/modules/security/security-routing.module.ts
+++ src/app/modules/security/security-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContextComponent} from '../process/context/context.component';
+import {HomepageComponent} from '../process/compponets/homepage/homepage.component';
+import {SecurityContextComponent} from './componnent/security-context/security-context.component';
+import {LoginComponent} from './componnent/login/login.component';
+import {SignupComponent} from './componnent/signup/signup.component';
+import {OtpVerificationComponent} from './componnent/otp-verification/otp-verification.component';
+import {ResetPasswordComponent} from './componnent/reset-password/reset-password.component';
+import {ForgotPasswordComponent} from './componnent/forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '',redirectTo:'/security/context',pathMatch:'full'},
+  {path:'context' , component:SecurityContextComponent, children: [
+      {path : '',redirectTo:'/security/context/login',pathMatch:'full'},
+      {path :'login', component:LoginComponent},
+      {path :'signup', component:SignupComponent},
+      {path :'confirm-otp', component:OtpVerificationComponent},
+      {path :'reset-password', component:ResetPasswordComponent},
+      {path :'forgot-password', component:ForgotPasswordComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
